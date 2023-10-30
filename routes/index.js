385898d43936e8fa01e1e62a8f81291b32f176fe
Yosi Fridman <yosi@alterno.io>
2023-10-30 09:43:59 +0200

    iroads

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 const router = express.Router();
 
+const title = "IRoads Node.js server";
+const description = `Service is up and running on Platform: ${process.platform}, Node version ${process.version}`;
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title, description });
 });
 
 export default router;
